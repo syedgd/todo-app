340eb5398d1cb39d68c3bfcef3a585fab494519a
Syed GD <syedgd@gmail.com>
2017-11-11 16:20:33 +0500

    action buttons event handlers implemented with bugs

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class TodoItem extends Component {
     
   
     return (
-      <div className="TodoItem">
+      <div className={"TodoItem" + (this.props.todo.isDone ? ' Done' : '')}>
         
             <input type='checkbox' checked={isDone} onChange={this.updateTodo()}  value= {this.props.todo.id} /> {this.props.todo.title}
         
@@ -63,6 +63,29 @@ class TodoList extends Component {
   }
 }
 
+class ActionsBar extends Component {
+
+  showFilteredTodos(isDone) {
+    
+    this.props.buttonClick(isDone);
+  }
+
+  render() {
+    return(
+      <div className="TodoItem ActionsBar">
+        <div className="buttonsContainer">
+          <table className="buttonsContainer">
+            <tr className="buttonsContainer">
+              <td><a href="#"><span className="button">All</span></a></td>
+              <td><a href="#" onClick={this.showFilteredTodos(true)}><span className="button">Completed</span></a></td>
+              <td><a href="#" onClick={this.showFilteredTodos(false)}><span className="button">Pending</span></a></td>
+          </tr>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
 class App extends Component {
   constructor() {
     super();
@@ -81,6 +104,13 @@ class App extends Component {
     )
   }
 
+  showFilteredTodos(pIsDone) {
+    var filteredTodos = this.state.todos.filter(todo => todo.isDone==pIsDone);
+    this.setState({
+      todos: filteredTodos,
+    });
+  }
+
 
 
   render() {
@@ -89,6 +119,7 @@ class App extends Component {
         <TodoAppHeader />
         <NewTodoItem />
         <TodoList todos = {this.state.todos} onClick={ todo => this.updateTodo(todo)} />
+        <ActionsBar buttonClick={isDone=>this.showFilteredTodos(isDone)} />
       </div>
     );
   }
